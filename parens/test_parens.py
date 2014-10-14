f16d142a79c4b6a34ae04a440a13280a405a61fb
Pat Prendergast <pat@wetdog.net>
2014-10-14 11:49:26 -0700

    testing finished.

--- parens/test_parens.py
+++ parens/test_parens.py
@@ -2,17 +2,22 @@ import pytest
 import parens
 
 
-def test_parens_check(text):
+def test_parens_check():
     testing1 = "I (hate) extra('s in my (strings."
-    assert parens_check(testing1) == 1
+    assert parens.parens_check(testing1) == 1
     testing2 = "I said) I hate ) extra (parens) in my strings."
-    assert parens_check(testing1) == -1
+    assert parens.parens_check(testing2) == -1
     testing3 = "I (mean) it."
-    assert parens_check(testing1) == 0
+    assert parens.parens_check(testing3) == 0
     testing4 = "well )( what about that."
-    assert parens_check(testing1) == -1
+    assert parens.parens_check(testing4) == -1
     testing5 = "Sometimes there aren't any."
-    assert = parens_check(testing5) == 0
+    assert parens.parens_check(testing5) == 0
+    testing6 = "Sometimes it's (weird)(."
+    assert parens.parens_check(testing6) == 1
     testing6 = 5555
-    #assert parens_check(testing6)
-    #Attribute Error
+    
+def test_parens_error():
+    with pytest.raises(AttributeError):
+        parens.parens_check(555)
+    
